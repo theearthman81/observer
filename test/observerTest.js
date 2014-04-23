4ec791c6e87f0ecc490294bc5b9030eb8c5f8fd9
jenglish81 <jimenglish81@gmail.com>
2014-04-23 21:11:02 +0100

    adding basic tests

--- test/observerTest.js
+++ test/observerTest.js
@@ -1,7 +1,28 @@
 "use strict";
  
 describe('ObserverTest', function(){
-    it('foo', function(){
-        expect('foo').toBe('foo');
+   var observer;
+   
+   beforeEach(function() {
+      observer = new Observer();
     });
+   
+   it('can be subscribed to once', function(){
+       var spy = jasmine.createSpy();
+       
+       observer.subscribe('foo', spy);
+       observer.publish('foo');
+       expect(spy).toHaveBeenCalled();
+   });
+   
+   it('can be subscribed to more than once', function(){
+       var spy = jasmine.createSpy();
+       var secondSpy = jasmine.createSpy();
+       
+       observer.subscribe('foo', spy);
+       observer.subscribe('foo', secondSpy);
+       observer.publish('foo');
+       expect(spy).toHaveBeenCalled();
+       expect(secondSpy).toHaveBeenCalled();
+   });
 });
